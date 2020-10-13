b9ebeeeae1558945ff6fc69b9c0b403277c2e679
Nicolas <nicolas.lozano.282002@gmail.com>
2020-10-12 22:39:12 -0300

    .

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using CompAndDel.Pipes;
 using CompAndDel.Filters;
-
+using TwitterUCU;
 
 namespace CompAndDel
 {
@@ -24,6 +24,15 @@ namespace CompAndDel
             PictureProvider t = new PictureProvider();
             t.SavePicture(pic2,"..\\..\\ImagenPruebas1.jpg");
             t.SavePicture(pic3,"..\\..\\ImagenPruebas2.jpg");
+            
+            string consumerKey = "620e818a46524ceb92628cde08068242";
+            string consumerKeySecret = "6rc35cHCyqFQSy4vIIjKiCYu31qqkBBkSS5BRlqeYCt5r7zO5B";
+            string accessTokenSecret = "gNytQjJgLvurJekVU0wmBBkrR1Th40sJmTO8JDhiyUkuy";
+            string accessToken = "1396065818-MeBf8ybIXA3FpmldORfBMdmrVJLVgijAXJv3B18";
+            
+            var twitter = new TwitterMessage(consumerKey, consumerKeySecret, accessToken, accessTokenSecret);
+            twitter.SendMessage("Hola!", "2654205941");
+
         }
     }
 }
